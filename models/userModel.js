3975e12abf446a1dbf90e5bafb78df43ba3ad302
Tej7278-chippada <tejachippada45@gmail.com>
2024-10-18 22:06:19 +0530

    userModel upadated for hashed password not generated properly

--- models/userModel.js
+++ models/userModel.js
@@ -11,13 +11,24 @@ const userSchema = new mongoose.Schema({
 // Hash the password before saving the user
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next(); // Only hash if the password is new or modified
-  this.password = await bcrypt.hash(this.password, 12); // Hashing with salt rounds
-  next();
+  try {
+    this.password = await bcrypt.hash(this.password, 12); // Hashing the password with 12 salt rounds
+    console.log('Hashed password:', this.password); // Log the hashed password for debugging
+    next();
+  } catch (err) {
+    return next(err);
+  }
 });
 
 // Method to compare input password with hashed password
 userSchema.methods.comparePassword = async function (inputPassword) {
-  return await bcrypt.compare(inputPassword, this.password); // Returns true if password matches
+  try {
+    const isMatch = await bcrypt.compare(inputPassword, this.password);
+    console.log('Password match result:', isMatch); // Log the result of the comparison
+    return isMatch;
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 const User = mongoose.model('User', userSchema);
